8246412bb3c60db8aaf01fee2c4d134897b0228b
Antony <Bailey>
2013-02-25 18:45:01 +0000

    Initial Commit
    Please ignore code is pretty rough

---
+++ PseudonymMatching/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PseudonymMatching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenCsv = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCheckPsudonym = new System.Windows.Forms.Button();
+            this.txtPseudonym = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenCsv
+            // 
+            this.btnOpenCsv.Location = new System.Drawing.Point(114, 455);
+            this.btnOpenCsv.Name = "btnOpenCsv";
+            this.btnOpenCsv.Size = new System.Drawing.Size(98, 23);
+            this.btnOpenCsv.TabIndex = 0;
+            this.btnOpenCsv.Text = "Open CSV File";
+            this.btnOpenCsv.UseVisualStyleBackColor = true;
+            this.btnOpenCsv.Click += new System.EventHandler(this.btnOpenCsv_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(893, 424);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnCheckPsudonym
+            // 
+            this.btnCheckPsudonym.Location = new System.Drawing.Point(609, 455);
+            this.btnCheckPsudonym.Name = "btnCheckPsudonym";
+            this.btnCheckPsudonym.Size = new System.Drawing.Size(148, 23);
+            this.btnCheckPsudonym.TabIndex = 2;
+            this.btnCheckPsudonym.Text = "Check Pseudonym";
+            this.btnCheckPsudonym.UseVisualStyleBackColor = true;
+            this.btnCheckPsudonym.Click += new System.EventHandler(this.btnCheckPsudonym_Click);
+            // 
+            // txtPseudonym
+            // 
+            this.txtPseudonym.Location = new System.Drawing.Point(468, 457);
+            this.txtPseudonym.Name = "txtPseudonym";
+            this.txtPseudonym.Size = new System.Drawing.Size(100, 20);
+            this.txtPseudonym.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 500);
+            this.Controls.Add(this.txtPseudonym);
+            this.Controls.Add(this.btnCheckPsudonym);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOpenCsv);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenCsv;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCheckPsudonym;
+        private System.Windows.Forms.TextBox txtPseudonym;
+    }
+}
+
---
+++ PseudonymMatching/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using PseudonymMatching.Properties;
+
+namespace PseudonymMatching
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ControlsState(false);
+        }
+
+        private void ControlsState(Boolean state)
+        {
+            txtPseudonym.Enabled = state;
+            btnCheckPsudonym.Enabled = state;
+        }
+
+        private void btnOpenCsv_Click(object sender, EventArgs e)
+        {
+            var comma = ",";
+            var tablename = "Persons";
+            var dataset = new DataSet();
+            using (var openFileDialog1 = new OpenFileDialog
+                {
+                    Filter = "CSV|*.csv",
+                    FilterIndex = 1
+                })
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var filename = openFileDialog1.FileName;
+                    var sr = new StreamReader(filename);
+                    var csv = File.ReadAllText(openFileDialog1.FileName);
+                    dataset.Tables.Add(tablename);
+                    dataset.Tables[tablename].Columns.Add("Forename");
+                    dataset.Tables[tablename].Columns.Add("Gender");
+                    dataset.Tables[tablename].Columns.Add("Occurance");
+
+                    var allData = sr.ReadToEnd();
+                    var rows = allData.Split("\r".ToCharArray());
+
+                    for (var i = 0; i < rows.Length; i++)
+                    {
+                        var r = rows[i].Replace("\"", "");
+                        var items = r.Split(comma.ToCharArray());
+                        dataset.Tables[tablename].Rows.Add(items);
+                    }
+                    if (dataGridView1 != null)
+                        if (dataset.Tables != null) dataGridView1.DataSource = dataset.Tables[0].DefaultView;
+                    ControlsState(true);
+
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong");
+                }
+            }
+        }
+
+        private void btnCheckPsudonym_Click(object sender, EventArgs e)
+        {
+            var pn = txtPseudonym.Text.ToString(CultureInfo.InvariantCulture);
+
+            if (String.IsNullOrWhiteSpace(pn))
+            {
+                MessageBox.Show("Invalid Name");
+            }
+            else
+            {
+                //if (pn == "Richard")
+                {
+                    var possibilities = "Richard,Dick,Dicky,Dickie,Rich,Rick,Richy";
+
+                    var posarray = possibilities.Split(",".ToCharArray());
+                    //}
+
+
+                    foreach (var name in posarray)
+                    {
+                        var matchedRows = dataGridView1.Rows
+                                                       .Cast<DataGridViewRow>()
+                                                       .Select(r =>
+                                                           {
+                                                               var value = r.Cells["Forename"].Value;
+                                                               return value ?? null;
+                                                           })
+                                                       .Where(v => v != null && v.ToString() == name)
+                                                       .ToList();
+                    }
+
+                }
+            }
+        }
+    }
+
+}
