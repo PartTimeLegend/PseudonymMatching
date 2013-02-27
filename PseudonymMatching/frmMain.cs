238007a7885d93e7b49db1fd9e23bf981e3492b8
Antony <Bailey>
2013-02-27 15:53:59 +0000

    Documented code with comments
    May take a while to run with large dataset

--- PseudonymMatching/frmMain.cs
+++ PseudonymMatching/frmMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
@@ -19,57 +20,79 @@ namespace PseudonymMatching
 
         private void ControlsState(Boolean state)
         {
+            // Enable / Disable Controls
             btnOpenPseudonymFile.Enabled = state;
         }
+        private void IncrementProgressBar(int inc)
+        {
+            // Increment Progress Bar as application progresses
+            // Shows user that application has not crashed
+            progressBar1.Increment(inc);
+        }
 
+        #region Open CSV file and populate datagridview
         private void btnOpenCsv_Click(object sender, EventArgs e)
         {
+            // Set constants for use
             const string comma = ",";
             const string tablename = "Persons";
+            // Store CSV to a dataset
             var dataset = new DataSet();
+            // Start using the openFileDialog1
             using (var openFileDialog1 = new OpenFileDialog
                 {
-                    Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv,
+                    // Set filter to only allow CSV files
+                    Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv, // Text strings moved to resource file. This helps with cross language support.
                     FilterIndex = 1
                 })
             {
+                // Check that a valid file is opened
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     var filename = openFileDialog1.FileName;
-                    if (filename == null) throw new ArgumentNullException("filename");
+                    if (filename == null) throw new ArgumentNullException("filename"); // Just in case no file has got through.
+                    // Use StreamReader to open file
                     var sr = new StreamReader(filename);
-                    File.ReadAllText(openFileDialog1.FileName);
-                    dataset.Tables.Add(tablename);
-                    dataset.Tables[tablename].Columns.Add("Forename");
+                    File.ReadAllText(openFileDialog1.FileName); // Read whole file
+                    dataset.Tables.Add(tablename); // Add table to dataset
+                    // Column names
+                    dataset.Tables[tablename].Columns.Add("Forename"); 
                     dataset.Tables[tablename].Columns.Add("Gender");
                     dataset.Tables[tablename].Columns.Add("Occurance");
                     progressBar1.Increment(1);
-                    var allData = sr.ReadToEnd();
-                    var rows = allData.Split("\r".ToCharArray());
-                    progressBar1.Increment(1);
+                    var allData = sr.ReadToEnd(); // Read all of file
+                    var rows = allData.Split(Environment.NewLine.ToCharArray()); // Split rows on line break
+                    IncrementProgressBar(1);
+                    // Loop through rows
                     for (var i = 0; i < rows.Length; i++)
                     {
-                        var r = rows[i].Replace("\"", "");
-                        var items = r.Split(comma.ToCharArray());
-                        dataset.Tables[tablename].Rows.Add(items);
+                        var r = rows[i].Replace("\"", ""); // Remove " from any cells
+                        var items = r.Split(comma.ToCharArray()); // Split row on comma to an array
+                        dataset.Tables[tablename].Rows.Add(items); // Insert row into datatable
+                        IncrementProgressBar(1);
                     }
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
+                    // Sanity checks
                     if (dataGridView1 != null)
-                        if (dataset.Tables != null) dataGridView1.DataSource = dataset.Tables[0].DefaultView;
-                    ControlsState(true);
-                    progressBar1.Increment(1);
+                        if (dataset.Tables != null) dataGridView1.DataSource = dataset.Tables[0].DefaultView; // Set datasource of datagridview
+                    ControlsState(true); // Enable controls
+                    IncrementProgressBar(1);
 
                 }
                 else
                 {
+                    // No file selected
                     MessageBox.Show(Resources.Form1_btnOpenCsv_Click_Something_went_wrong);
                 }
             }
         }
-
+        #endregion
+        
         private void btnCheckPsudonym_Click(object sender, EventArgs e)
         {
+
+            #region Old Code - Commented Out - Left to show progression of design
             //var searchValue = "A,A J";
             //var searchValueArray = searchValue.Split(",".ToCharArray());
 
@@ -98,75 +121,93 @@ namespace PseudonymMatching
             //}
             //MessageBox.Show("Completed");
         }
-
+            #endregion
+        #region Open Pseudonym File
         private void btnOpenPseudonymFile_Click(object sender, EventArgs e)
         {
+            // Set constants
             const string comma = ",";
             const string tablename = "Pseudonyms";
-            var dataset = new DataSet();
+            var dataset = new DataSet(); // Another dataset
+            // Use openFileDialog - Could have reused first, but kept seperate for future adaptation
             using (var openFileDialog2 = new OpenFileDialog
                 {
-                    Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv,
+                    Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv, // Text again moved to resource file
                     FilterIndex = 1
                 })
             {
+                // Check valid file is opened
                 if (openFileDialog2.ShowDialog() == DialogResult.OK)
                 {
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     var filename = openFileDialog2.FileName;
-                    var sr = new StreamReader(filename);
+                    // Read in file
+                    var sr = new StreamReader(filename); 
                     File.ReadAllText(openFileDialog2.FileName);
+                    // Create datatable and columns
                     dataset.Tables.Add(tablename);
                     dataset.Tables[tablename].Columns.Add("Basename");
                     dataset.Tables[tablename].Columns.Add("Pseudonyms");
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     var allData = sr.ReadToEnd();
-                    var rows = allData.Split("\r".ToCharArray());
-
+                    var rows = allData.Split(Environment.NewLine.ToCharArray()); // Split rows on line break
+                    // Loop through all rows
                     for (var i = 0; i < rows.Length; i++)
                     {
-                        var r = rows[i].Replace("\"", "");
-                        var items = r.Split(comma.ToCharArray());
-                        dataset.Tables[tablename].Rows.Add(items);
+                        var r = rows[i].Replace("\"", ""); // Remove " from lines
+                        var items = r.Split(comma.ToCharArray()); // Split row on commas to an array
+                        dataset.Tables[tablename].Rows.Add(items); // Insert into datatable
+                        IncrementProgressBar(1);
                     }
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     //var searchValueArray = searchValue.Split(",".ToCharArray());
 
+                    // Inst new DataView to do comparisons with
                     var dv = new DataView {Table = dataset.Tables[tablename]};
 
+                    // Loop through all rows in dataview
                     for (var i = 0; i < dv.Table.Rows.Count;i++)
                     {
                         //var baseName = dataset.Tables[tablename].Columns["Basename"];
+                        
+                        var baseName = dv.Table.Rows[i][0].ToString().Replace("\n",""); // Remove "
+                        var possibleMatches = dv.Table.Rows[i][1].ToString().Split("|".ToCharArray()); // Split match options on |
 
-                        var baseName = dv.Table.Rows[i][0].ToString().Replace("\n","");
-                        var possibleMatches = dv.Table.Rows[i][1].ToString().Split("|".ToCharArray());
-
+                        // Create list of rows found
                         var rowsFound = new List<int>();
-
+                        // Cannot implicitly type a null - I need a string in outer scope
                         string pos = null;
+                        IncrementProgressBar(1);
+                        // Loop through possible matches
                         for (var index1 = 0; index1 < possibleMatches.Length; index1++)
                         {
+                            IncrementProgressBar(1);
                             pos = possibleMatches[index1];
+                            // Loop through datagridview rows
                             for (var l = 0; l < dataGridView1.Rows.Count; l++)
                             {
-                                var row = dataGridView1.Rows[l];
-                                var value = row.Cells[0].Value;
+                                IncrementProgressBar(1);
+                                var row = dataGridView1.Rows[l]; // Set row
+                                var value = row.Cells[0].Value; // Get value of First Name cell
                                 if (value != null &&
-                                    (value.ToString().Replace("\n", "") == pos.ToUpper()))
+                                    (value.ToString().Replace(Environment.NewLine, "") == pos.ToUpper())) // Treat all as uppercase
                                 {
                                     i = row.Index;
                                     //MessageBox.Show(i.ToString());
-                                    rowsFound.Add(i);
-                                    progressBar1.Increment(1);
+                                    Debug.Print(i.ToString());
+                                    rowsFound.Add(i); // Add to list
+                                    IncrementProgressBar(1);
                                 }
                             }
                         }
+                        // Loop through rows found list
                         foreach (var i1 in rowsFound)
                         {
+                            // Display message to user to see found duplicate
                             MessageBox.Show(Resources.Form1_btnOpenPseudonymFile_Click_A_possible_pseudonym_has_been_discovered_in_the_original_data_on_row_ + i1.ToString()+Resources.Form1_btnOpenPseudonymFile_Click_);
-
-                            // Magic time
-
+                            IncrementProgressBar(1);
+                            // Matching Code
+                            // Table constants, dataset and columns
                             const string finalTable = "Results";
                             var finalDataset = new DataSet();
                             finalDataset.Tables.Add(finalTable);
@@ -177,11 +218,15 @@ namespace PseudonymMatching
                             finalDataset.Tables[finalTable].Columns.Add("Pseudonym");
                             progressBar1.Increment(1);
                             //var values = "";
-
+                            // Check datagridview is not null
                             if (dataGridView1 != null)
                             {
+                                IncrementProgressBar(1);
+                                // Check there are enough rows
                                 if (dataGridView1.Rows.Count > i1)
                                 {
+                                    IncrementProgressBar(1);
+                                    // Match values to sources
                                     var originalName = dataGridView1.Rows[i1].Cells[0].Value.ToString();
                                     var gender = dataGridView1.Rows[i1].Cells[1].Value.ToString();
                                     var rowId = i1.ToString(CultureInfo.InvariantCulture);
@@ -189,45 +234,49 @@ namespace PseudonymMatching
                                     var pseudonym = pos;
 
                                     var resultsArray = new string[5];
-                                    resultsArray[0] = originalName.Replace("\n","");
-                                    resultsArray[1] = gender.Replace("\n", "");
-                                    resultsArray[2] = rowId.Replace("\n", "");
-                                    resultsArray[3] = bN.Replace("\n", "");
-                                    if (pseudonym != null) resultsArray[4] = pseudonym.Replace("\n", "");
-
+                                    resultsArray[0] = originalName.Replace(Environment.NewLine,"");
+                                    resultsArray[1] = gender.Replace(Environment.NewLine, "");
+                                    resultsArray[2] = rowId.Replace(Environment.NewLine, "");
+                                    resultsArray[3] = bN.Replace(Environment.NewLine, "");
+                                    if (pseudonym != null) resultsArray[4] = pseudonym.Replace(Environment.NewLine, "");
+                                    // Add to dataset
                                     finalDataset.Tables[finalTable].Rows.Add(resultsArray);
-                                    progressBar1.Increment(1);
+                                    IncrementProgressBar(1);
                                 }
                             }
-
+                            // Final sanity check
                             if (dataGridView1 != null)
-                                dataGridView1.DataSource = finalDataset.Tables[finalTable].DefaultView;
-                            progressBar1.Increment(1);
+                                dataGridView1.DataSource = finalDataset.Tables[finalTable].DefaultView; // Populate datagrid with values
+                            IncrementProgressBar(1);
                         }
 
                     }
-
+                    // Inform user the process has completed
                     MessageBox.Show(Resources.Form1_btnOpenPseudonymFile_Click_Completed);
-                    progressBar1.Value = 0;
+                    IncrementProgressBar(1);
 
                 }
                 else
                 {
+                    // Invalid or no file
                     MessageBox.Show(Resources.Form1_btnOpenCsv_Click_Something_went_wrong);
                 }
             }
-
+        #endregion
         }
-
+        #region Form Init
         private void frmMain_Load(object sender, EventArgs e)
         {
-
+            // Avoid using form load
         }
-
+        #endregion
+        #region Launch Modify Pseudonym File
         private void btnModifyPseudonymFile_Click(object sender, EventArgs e)
         {
-            var foo = new frmModify();
-            foo.Show();
+            // Instanciate form and display
+            var frm = new frmModify();
+            frm.Show();
         }
+        #endregion
     }
 }
--- PseudonymMatching/frmModify.cs
+++ PseudonymMatching/frmModify.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,45 +19,60 @@ namespace PseudonymMatching
             InitializeComponent();
         }
 
+        private void IncrementProgressBar(int inc)
+        {
+            // Increment Progress Bar as application progresses
+            // Shows user that application has not crashed
+            progressBar1.Increment(inc);
+        }
+
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            // Set constants and dataset
             const string comma = ",";
             const string tablename = "Data";
             var dataset = new DataSet();
+            // Use openFileDialog like before
             using (var openFileDialog1 = new OpenFileDialog
             {
-                Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv,
+                Filter = Resources.Form1_btnOpenPseudonymFile_Click_CSV___csv, // Again in resources file
                 FilterIndex = 1
             })
             {
+                // Check file opened is valid
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     var filename = openFileDialog1.FileName;
                     if (filename == null) throw new ArgumentNullException("filename");
+                    // Open file and read contents
                     var sr = new StreamReader(filename);
                     File.ReadAllText(openFileDialog1.FileName);
+                    // Add table and columns to dataset
                     dataset.Tables.Add(tablename);
                     dataset.Tables[tablename].Columns.Add("Basename");
                     dataset.Tables[tablename].Columns.Add("Pseudonyms");
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     var allData = sr.ReadToEnd();
-                    var rows = allData.Split("\r".ToCharArray());
-                    progressBar1.Increment(1);
+                    var rows = allData.Split(Environment.NewLine.ToCharArray());
+                    IncrementProgressBar(1);
+                    // Loop through rows
                     for (var i = 0; i < rows.Length; i++)
                     {
-                        var r = rows[i].Replace("\"", "");
-                        var items = r.Split(comma.ToCharArray());
-                        dataset.Tables[tablename].Rows.Add(items);
+                        IncrementProgressBar(1);
+                        var r = rows[i].Replace("\"", ""); // Remove "
+                        var items = r.Split(comma.ToCharArray()); // Split on CSV
+                        dataset.Tables[tablename].Rows.Add(items); // Add to dataset table
                     }
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
                     if (dataGridView1 != null)
-                        if (dataset.Tables != null) dataGridView1.DataSource = dataset.Tables[0].DefaultView;
-                    progressBar1.Increment(1);
+                        if (dataset.Tables != null) dataGridView1.DataSource = dataset.Tables[0].DefaultView; // Populate datagrid
+                    IncrementProgressBar(1);
 
                 }
                 else
                 {
+                    // Invalid file or non selected
                     MessageBox.Show(Resources.Form1_btnOpenCsv_Click_Something_went_wrong);
                 }
             }
@@ -65,31 +81,38 @@ namespace PseudonymMatching
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            var saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.DefaultExt = ".csv";
-            saveFileDialog1.Filter = "CSV|.csv";
-            var exportData = "";
+            // Inst saveFileDialog
+            var saveFileDialog1 = new SaveFileDialog {DefaultExt = ".csv", Filter = "CSV|.csv"};
+            var exportData = string.Empty;
+            // Check file name is valid
             if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
             var fileName = saveFileDialog1.FileName;
+            // Loop through datagridview rows
             foreach (DataGridViewRow dr in dataGridView1.Rows)
             {
-                progressBar1.Increment(1);
+                IncrementProgressBar(1);
+                // Now loop through cells
                 foreach (DataGridViewCell dc in dr.Cells)
                 {
-                    progressBar1.Increment(1);
+                    IncrementProgressBar(1);
+                    // Check has value and assign
                     if (dc.Value != null)
                     {
-                        exportData += dc.Value.ToString() + ",";
+                        exportData += dc.Value.ToString() + ","; // Split on comma. Previously used a constant for this
                     }
                 }
-                progressBar1.Increment(1);
-                exportData += Environment.NewLine.ToString();
-                exportData = exportData.Substring(0, exportData.Length - 3) + Environment.NewLine.ToString();
+                IncrementProgressBar(1);
+                exportData += Environment.NewLine.ToString(CultureInfo.InvariantCulture); // Add some line breaks
+                exportData = exportData.Substring(0, exportData.Length - 3) + Environment.NewLine.ToString(CultureInfo.InvariantCulture);
+                // Inst StreamWriter
                 var sw = new System.IO.StreamWriter(fileName);
-                progressBar1.Increment(1);
+                IncrementProgressBar(1);
+                // Write the file
                 sw.Write(exportData);
+                // Close the file
                 sw.Close();
-                progressBar1.Value = 0;
+                IncrementProgressBar(1);
+                // Tell user is has saved so they are not waiting
                 MessageBox.Show(Resources.frmModify_btnSave_Click_Saved);
             }
         }
